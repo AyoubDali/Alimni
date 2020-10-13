3debe3e06ce9fd4a62ec2035c53871eadbd05387
Ayoub Dali <ayoub.dali@ensi-uma.tn>
2020-10-13 11:36:49 +0200

    add image detection api

--- AlimniServer/app.py
+++ AlimniServer/app.py
@@ -113,7 +113,6 @@ def get_detections():
         })
         img = cv2.cvtColor(raw_img.numpy(), cv2.COLOR_RGB2BGR)
         img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
-        #print('output saved to: {}'.format(output_path + 'detection' + str(num) + '.jpg'))
 
     #remove temporary images
     for name in image_names:
@@ -122,6 +121,45 @@ def get_detections():
         return jsonify({"response":response}), 200
     except FileNotFoundError:
         abort(404)
+
+
+# API that returns image with detections on it
+@app.route('/image', methods= ['POST'])
+def get_image():
+    image = request.files["images"]
+    image_name = image.filename
+    image.save(os.path.join(os.getcwd(), image_name))
+    img_raw = tf.image.decode_image(
+        open(image_name, 'rb').read(), channels=3)
+    img = tf.expand_dims(img_raw, 0)
+    img = transform_images(img, size)
+
+    t1 = time.time()
+    boxes, scores, classes, nums = yolo(img)
+    t2 = time.time()
+    output_path='/'
+    for i in range(nums[0]):
+        print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
+                                        np.array(scores[0][i]),
+                                        np.array(boxes[0][i])))
+    img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
+    img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
+    cv2.imwrite(output_path + 'detection.jpg', img)
+    
+    # prepare image for response
+    _, img_encoded = cv2.imencode('.png', img)
+    response = img_encoded.tostring()
+    
+    #remove temporary image
+    os.remove(image_name)
+
+    try:
+        return Response(response=response, status=200, mimetype='image/png')
+    except FileNotFoundError:
+        abort(404)
+
+
+
 
 
 if __name__ == '__main__':
